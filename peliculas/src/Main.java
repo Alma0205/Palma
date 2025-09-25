2b3ddb9e2c48c97a3390d1f4d26356365a3d9c07
Alma0205 <almacrespa10@gmail.com>
2025-09-25 16:21:46 -0500

    Update Main.java

--- peliculas/src/Main.java
+++ peliculas/src/Main.java
@@ -1,26 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-
         System.out.println(Reproducible.obtenerInfoLibreria());
         System.out.println("=====================================================");
 
-
+   
         Pelicula pelicula1 = new Pelicula("Nosferatu", "F. W. Murnau");
         Pelicula pelicula2 = new Pelicula("El Resplandor", "Stanley Kubrick");
         Pelicula pelicula3 = new Pelicula("Drácula", "Tod Browning");
 
- 
+     
         Serie serie1 = new Serie("Historias de Halloween", 1);
         Serie serie2 = new Serie("El Exorcista", 2);
 
-
+       
         ReproductorMultimedia miReproductor = new ReproductorMultimedia();
 
         System.out.println("--- Probando con Películas ---");
         miReproductor.gestionar(pelicula1);
         miReproductor.gestionar(pelicula2);
         miReproductor.gestionar(pelicula3);
-
 
         System.out.println("\n--- Probando con Series ---");
         miReproductor.gestionar(serie1);
