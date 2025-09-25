e1fdfea26f56e818aac883bd366c68942ae98690
Alma0205 <almacrespa10@gmail.com>
2025-09-25 16:23:06 -0500

    update

--- peliculas/src/ReproductorMultimedia.java
+++ peliculas/src/ReproductorMultimedia.java
@@ -1,8 +1,11 @@
 public class ReproductorMultimedia {
+    
+
     public void gestionar(Reproducible medio) {
         medio.reproducir();
         medio.adelantar(15);
         medio.pausar();
         medio.detener();
+        System.out.println("==========================================");
     }
 }
